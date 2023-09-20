b44856cb6f96db82cd685ee2397c5caaaa8a975c
shazarb <shazarb@umich.edu>
2023-09-20 12:40:03 -0400

    added str method

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -15,8 +15,8 @@ class Rectangle():
     # It returns a string, 
     #       "A rectangle with width ____ and height ____"
 
-    # YOUR CODE HERE
-
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
 
     # Create the "verify_input" method
