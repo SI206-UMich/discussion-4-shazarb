292b4173bac97574f2437281ab8576c92e011349
shazarb <shazarb@umich.edu>
2023-09-20 12:32:10 -0400

    add shalin

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -5,6 +5,7 @@ class Rectangle():
     # It sets an instance variable, "width" to the passed argument, width
     # It sets an instance variable, "height" to the passed argument, height
 
+#shalin
     # YOUR CODE HERE
 
 
